1ae67e25782255dc628acb64e96f4ddb0a41a96b
Philipp van Kempen <philipp.van-kempen@tum.de>
2019-04-01 08:54:40 +0200

    fix conflict in install_kernel.sh

--- install_kernel.sh
+++ install_kernel.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-<<<<<<< HEAD
-KERNEL_DIR=.
-
-./install_modules.sh
-=======
 ROOT_DIR="/home/foo/Android7.1/kernel/hardkernel/odroidxu3"
 
 if [ $# -eq 0 ]
@@ -14,7 +9,6 @@ then
 fi
 
 PRODUCT_BOARD=$1
->>>>>>> bc18e225419b3c97790eab7ea4d2e8ef2b2dc575
 
 adb $ANDROID_DEVICE reboot fastboot
 
